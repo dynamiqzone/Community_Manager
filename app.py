507b4e24880bb0fcc5ef7e33518fae070c35d42e
dynamiqzone <dynamiqzone@gmail.com>
2026-03-13 17:54:09 -0500

    fixed register ui and fields position and size

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Temporary route to preview ANY template
-@app.route('/preview/<page>')
-def preview(page):
+@app.route('/register')
+def register():
     # This will look for 'register.html' if you go to /preview/register.html
-    return render_template(page)
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
